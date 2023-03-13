82bc3649747660e796563a66453113d7e29b2c5b
Mike Patay <91127463+itsmikupaths@users.noreply.github.com>
2023-03-13 16:31:11 +0800

    homes creen and about screen

---
+++ src/views/AboutView.vue
@@ -0,0 +1,37 @@
+<template>
+  <main>
+    <h1>About our Pokedex</h1>
+
+    <p>Welcome to our Pokedex, the ultimate Pokemon Pokedex app! Our app is designed to be your one-stop-shop for everything related to Pokemon, from basic information about each Pokemon to detailed stats and evolution charts.</p>
+
+    <h2>Features</h2>
+    <ul>
+      <li>Detailed information about each Pokemon, including its type, abilities, and stats</li>
+      <li>Comprehensive Pokedex that includes all Pokemon up to the latest generation</li>
+      <li>Search function that allows you to quickly find any Pokemon by name or number</li>
+      <li>Favorite feature that lets you mark your favorite Pokemon for easy access</li>
+      <li>Evolution charts that show each Pokemon's evolution path and requirements</li>
+      <li>User-friendly interface that makes it easy to navigate and find the information you need</li>
+    </ul>
+  </main>
+
+  <a class="btn" href="/">Back</a>
+</template>
+
+<style scope>
+main {
+  margin-bottom: 10px;
+  color: var(--text);
+}
+h1, h2 {
+  color: var(--text-accent);
+}
+.btn {
+  border: 1px solid var(--text-accent);
+  border-radius: 5px;
+  padding: 5px;
+  text-decoration: none;
+  color: var(--text-accent);
+  width: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,53 @@
 <template>
   <main>
-    HomeView.vie
+    <div class="header">
+      welcome to
+      <p>Pokedex</p>
+    </div>
+    <img src="../assets/images/gen1-homescreen-removebg.png" alt="home screen image">
+    <nav>
+      <a href="/pokedex">Pokedex</a>
+      <a href="/about">About</a>
+    </nav>
   </main>
 </template>
+
+<style scope>
+main {
+  color: var(--text);
+}
+.header {
+  margin-bottom: 80px;
+}
+.header p {
+  color: var(--text-accent);
+  font-size: 42px;
+  font-weight: 700;
+  line-height: 42px;
+  margin: 0 0;
+}
+img {
+  width: 100%;
+}
+nav {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-top: 60px;
+}
+a {
+  border: 1px solid var(--text-accent);
+  border-radius: 5px;
+  color: var(--text);
+  text-decoration: none;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  font-size: 20px;
+  font-weight: 700;
+}
+a:hover {
+  background-color: var(--text-accent);
+  color: var(--text-grey);
+}
+</style>
